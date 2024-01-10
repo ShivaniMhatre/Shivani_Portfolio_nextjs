9d1fa11b5f0cba93f8719051ec02b302ed92a1a3
ShivaniMhatre <shivanikatkar97@gmail.com>
2024-01-10 13:28:08 +0530

    updates

--- portfolio/Component/MobileNav.tsx
+++ portfolio/Component/MobileNav.tsx
@@ -10,11 +10,11 @@ const MobileNav = ({ nav, closeNav }: Props) => {
     return (
         <div className={`fixed ${navAnimation} transform transition-all duration-300 top-0 bottom-0 right-0 left-0 z-[10000000] bg-[#09101a]`}>
             <div className='w-[100vw] h-[100vh] flex flex-col items-center justify-center'>
-                <div className='nav-link-mobile'>HOME</div>
-                <div className='nav-link-mobile'>SERVICES</div>
-                <div className='nav-link-mobile'>ABOUT</div>
-                <div className='nav-link-mobile'>PROJECT</div>
-                <div className='nav-link-mobile'>CONTACT</div>
+            <a href='#home' className='nav-link-mobile'>HOME</a>
+                <a href='#service' className='nav-link-mobile'>SERVICES</a>
+                <a href='#skill' className='nav-link-mobile'>SKILL</a>
+                <a href='#project' className='nav-link-mobile'>PROJECT</a>
+                <a href='#about' className='nav-link-mobile'>ABOUT</a>
                 <div onClick={closeNav}  className='absolute z-[100000000] cursor-pointer top-[2rem] right-[2rem] w-[2rem] l-[2rem] text-yellow-400'>
                     <XMarkIcon />
                 </div>
